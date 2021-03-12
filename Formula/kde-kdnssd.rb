c7beb24b157bdd2aaaac3147444eeb81a35690e2
Dawid Wróbel <me@dawidwrobel.com>
2021-03-11 21:02:23 -0400

    [kde-kdnssd] new formula

---
+++ Formula/kde-kdnssd.rb
@@ -0,0 +1,53 @@
+class KdeKdnssd < Formula
+  desc "Abstraction to system DNSSD features"
+  homepage "https://api.kde.org/frameworks/kdnssd/html/index.html"
+  url "https://download.kde.org/stable/frameworks/5.79/kdnssd-5.79.1.tar.xz"
+  sha256 "9716aeed43b794948a78444ea645ba14f24f13395d410a78a68b1f8428f51898"
+  license all_of: [
+    "BSD-3-Clause",
+    "LGPL-2.0-or-later",
+  ]
+  head "https://invent.kde.org/frameworks/kdnssd.git"
+
+  depends_on "cmake" => [:build, :test]
+  depends_on "doxygen" => :build
+  depends_on "graphviz" => :build
+  depends_on "kde-extra-cmake-modules" => [:build, :test]
+  depends_on "ninja" => :build
+
+  depends_on "qt@5"
+
+  def install
+    args = std_cmake_args
+    args << "-D BUILD_QCH=ON"
+    args << "-D BUILD_TESTING=OFF"
+    args << "-D BUILD_TESTS=OFF"
+    args << "-D BUILD_UNITTESTS=OFF"
+    args << "-D CMAKE_INSTALL_BUNDLEDIR=#{bin}"
+    args << "-D KDE_INSTALL_LIBDIR=lib"
+    args << "-D KDE_INSTALL_PLUGINDIR=lib/qt5/plugins"
+    args << "-D KDE_INSTALL_QMLDIR=lib/qt5/qml"
+    args << "-D KDE_INSTALL_QTPLUGINDIR=lib/qt5/plugins"
+
+    mkdir "build" do
+      system "cmake", "..", "-G", "Ninja", *args
+      system "ninja"
+      system "ninja", "install"
+      prefix.install "install_manifest.txt"
+    end
+  end
+
+  test do
+    (testpath/"CMakeLists.txt").write <<~EOS
+      project(test_KF5DNSSD)
+      find_package(Qt5 REQUIRED Core Network)
+      find_package(KF5DNSSD REQUIRED)
+    EOS
+
+    args = std_cmake_args
+    args << "-DQt5_DIR=#{Formula["qt@5"].opt_prefix/"lib/cmake/Qt5"}"
+
+    system "cmake", testpath.to_s, *args
+    system "make"
+  end
+end
