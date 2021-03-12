83017cb115de8e27ba4479b03e4f424f0ecf7137
Dawid Wróbel <me@dawidwrobel.com>
2021-03-11 21:02:23 -0400

    [kde-kwindowsystem] new formula

---
+++ Formula/kde-kwindowsystem.rb
@@ -0,0 +1,53 @@
+class KdeKwindowsystem < Formula
+  desc "Access to the windowing system"
+  homepage "https://api.kde.org/frameworks/kwindowsystem/html/index.html"
+  url "https://download.kde.org/stable/frameworks/5.79/kwindowsystem-5.79.0.tar.xz"
+  sha256 "b8aef806276b5f12e9810473679401a6715b3b86e786eb874b12b2b6ed16f196"
+  license all_of: [
+    "LGPL-2.1-or-later",
+    "MIT",
+    any_of: ["LGPL-2.1-only", "LGPL-3.0-only"],
+  ]
+  head "https://invent.kde.org/frameworks/kwindowsystem.git"
+
+  depends_on "cmake" => [:build, :test]
+  depends_on "doxygen" => :build
+  depends_on "graphviz" => :build
+  depends_on "kde-extra-cmake-modules" => [:build, :test]
+  depends_on "ninja" => :build
+
+  depends_on "qt@5"
+
+  def install
+    args = std_cmake_args
+    args << "-D BUILD_QCH=ON"
+    args << "-D BUILD_TESTING=OFF"
+    args << "-D BUILD_TESTS=OFF"
+    args << "-D BUILD_UNITTESTS=OFF"
+    args << "-D CMAKE_INSTALL_BUNDLEDIR=#{bin}"
+    args << "-D KDE_INSTALL_LIBDIR=lib"
+    args << "-D KDE_INSTALL_PLUGINDIR=lib/qt5/plugins"
+    args << "-D KDE_INSTALL_QMLDIR=lib/qt5/qml"
+    args << "-D KDE_INSTALL_QTPLUGINDIR=lib/qt5/plugins"
+
+    mkdir "build" do
+      system "cmake", "..", "-G", "Ninja", *args
+      system "ninja"
+      system "ninja", "install"
+      prefix.install "install_manifest.txt"
+    end
+  end
+
+  test do
+    (testpath/"CMakeLists.txt").write <<~EOS
+      project(test_KF5WindowSystem)
+      find_package(Qt5 REQUIRED Core Widgets)
+      find_package(KF5WindowSystem REQUIRED)
+    EOS
+
+    args = std_cmake_args
+    args << "-DQt5_DIR=#{Formula["qt@5"].opt_prefix/"lib/cmake/Qt5"}"
+    system "cmake", testpath.to_s, *args
+    system "make"
+  end
+end
